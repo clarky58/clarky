e926ebb8221538d980cd79d60cc1fc1f77f8ecd6
Cletus Ngwerume <cletuzz@gmail.com>
2023-11-17 14:24:37 +0200

    working admin

--- resources/views/admin/manage-files.blade.php
+++ resources/views/admin/manage-files.blade.php
@@ -54,6 +54,9 @@
                             <a  class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#lockFileModal{{ $file->id }}">
                                 Lock
                             </a>
+                            <a  class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteFileModal{{ $file->id }}">
+                                Delete
+                            </a>
                             @endif
                         </td>
                     <tr/>
@@ -130,6 +133,23 @@
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                                     <a href="{{ route('files.unlock', $file->id) }}" class="btn btn-primary">Yes, unlock</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal fade" id="deleteFileModal{{ $file->id }}" tabindex="-1" aria-labelledby="deleteFileModalLabel{{ $file->id }}" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="deleteFileModalLabel{{ $file->id }}">Delete File</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Are you sure you want to delete this file?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                                    <a href="{{ route('files.delete', $file->id) }}" class="btn btn-primary">Yes, delete</a>
                                 </div>
                             </div>
                         </div>
